aae0a3a1ab51f49176ed9bfcfc343d70636a18a0
swatishreesaho <swatisahoo641@gmail.com>
2024-08-05 11:56:13 +0530

    abcd

--- To-Do-Django-Backend/todoapp/models.py
+++ To-Do-Django-Backend/todoapp/models.py
@@ -1,23 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User,Group
-from django.db.models import Count
+from django.db.models import Count, JSONField
+from datetime import datetime
 # Create your models here.
+
+class Card(models.Model):
+    card_name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.card_name
 
 class Projects(models.Model):
     id = models.AutoField(primary_key=True)  
     name = models.CharField(max_length=255)
     deadline = models.DateField()
-    team = models.CharField(max_length=255)  
-    status = models.CharField(max_length=50)
+    team = models.ManyToManyField(User, related_name='project')
+    # team = models.CharField(max_length=255)  
+    # status = models.CharField(max_length=255)  
+    status = models.ForeignKey(Card, on_delete=models.CASCADE,default = "")
     manager = models.CharField(max_length=255, default="Default Manager")  
 
     def __str__(self):
         return self.name
     
-class Card(models.Model):
-    card_name = models.CharField(max_length=30)
-    def __str__(self):
-        return self.card_name
     
 class Task(models.Model):
     taskName = models.CharField(max_length=255,null=True, blank=True)
@@ -27,6 +31,8 @@ class Task(models.Model):
     assigned_groups = models.ManyToManyField(Group, related_name='group_tasks',blank=True)
     project = models.ForeignKey(Projects, on_delete=models.CASCADE,null=True)
     description = models.CharField(max_length=100,null=True,blank=True)
+    startdate = models.DateField(null=True, blank=True)
+    enddate = models.DateField(null=True, blank=True)
     deadline = models.DateField(null=True, blank=True)
     file = models.FileField(max_length=100, upload_to="media/", null=True, blank=True)
     Priority_High = 'high'
@@ -50,6 +56,34 @@ class Task(models.Model):
 
     # def __str__(self):
     #     return self.name
+
+    task_wallet = models.IntegerField(null=True,blank=True)
+    ETA = JSONField(default=list, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+    # Check if done_date has changed
+        if self.pk is not None:
+            orig = Task.objects.get(pk=self.pk)
+            if orig.deadline != self.deadline:
+                if self.ETA is None:
+                    self.ETA = []
+                # Append the new done_date as a string
+                self.ETA.append(self.deadline.strftime("%Y-%m-%d"))
+
+    # Set priority to high if ETA is within 3 days
+        if self.ETA:
+            today = datetime.now().date()
+            for eta_date_str in self.ETA:
+                eta_date = datetime.strptime(eta_date_str, "%Y-%m-%d").date()
+                if (eta_date - today).days <= 2:
+                    self.priority = self.Priority_High
+                    break
+
+        super(Task, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.taskName
+ 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -83,3 +117,9 @@ class UserProfile(models.Model):
 
     # current_status = models.CharField(max_length=30, default='Not Started')
 
+class Message(models.Model):
+    task = models.ForeignKey(Task,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    message = models.TextField(max_length=5000)
+    date_time = models.DateTimeField()
+ 
